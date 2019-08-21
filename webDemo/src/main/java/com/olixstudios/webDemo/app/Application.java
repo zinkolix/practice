0d7f4faee978e99a2e43f99c21e5c67e67393ff5
Ashwini <olixstudios@gmail.com>
2019-08-21 22:35:27 +0530

    Simple demo for the spring MVC

---
+++ webDemo/src/main/java/com/olixstudios/webDemo/app/Application.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.olixstudios.webDemo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ashwin
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ webDemo/src/main/java/com/olixstudios/webDemo/app/GreetingController.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.olixstudios.webDemo.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author Ashwin
+ *
+ */
+@Controller
+public class GreetingController {
+
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false,defaultValue="World") String name, Model model) {
+		model.addAttribute("name",name);
+		return "greeting";
+	}
+}
